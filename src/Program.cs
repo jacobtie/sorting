5f57e962547fcd5d602d1745c6b2d1ffca661a9a
masonreynolds <masonreynolds65@gmail.com>
2019-09-07 22:33:18 +0200

    Finished merge sort

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using assignment1.sorting;
 
 namespace assignment1
 {
@@ -6,10 +7,14 @@ namespace assignment1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] arr = {10, 6, 5, 4, 7, 3, 8, 9, 1, 2};
 
-			Console.WriteLine("\nPress enter to exit...");
-			Console.Read();
+			MergeSort.mergeSort(arr, 0, arr.Length-1);
+
+			foreach (int num in arr)
+			{
+				Console.WriteLine(num);
+			}
 		}
 	}
 }
--- src/sorting/MergeSort.cs
+++ src/sorting/MergeSort.cs
@@ -1,7 +1,72 @@
+using System;
+
 namespace assignment1.sorting
 {
 	public static class MergeSort
 	{
+		public static void mergeSort(int[] arr, int start, int end)
+		{
+			if (start < end)
+			{
+				int mid = (end + start) / 2;
 
+				mergeSort(arr, start, mid);
+				mergeSort(arr, mid+1, end);
+
+				merge(arr, start, mid, end);
+			}
+		}
+
+		public static void merge(int[] arr, int start, int mid, int end)
+		{
+			int sizeL = mid - start + 1;
+			int sizeR = end - mid;
+			int currL = 0;
+			int currR = 0;
+			int k = start;
+
+			int[] left = new int[sizeL];
+			int[] right = new int[sizeR];;
+
+			for (int i = 0; i < sizeL; i++)
+			{
+				left[i] = arr[start + i];
+			}
+
+			for (int j = 0; j < sizeR; j++)
+			{
+				right[j] = arr[mid + 1 + j];
+			}
+
+			while (currL < sizeL && currR < sizeR) 
+			{ 
+				if (left[currL] <= right[currR]) 
+				{ 
+					arr[k] = left[currL];
+					currL++;
+				} 
+				else
+				{ 
+					arr[k] = right[currR];
+					currR++;
+				} 
+
+				k++; 
+			} 
+
+			while (currL < sizeL) 
+			{ 
+				arr[k] = left[currL]; 
+				currL++; 
+				k++; 
+			} 
+			
+			while (currR < sizeR) 
+			{ 
+				arr[k] = right[currR]; 
+				currR++; 
+				k++; 
+			} 
+		}
 	}
 }
